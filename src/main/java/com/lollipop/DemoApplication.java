f0f6ef01c7aa4474bc6011bc113559ab27bf3759
zhubao1988 <727979738@qq.com>
2017-11-30 17:56:23 +0800

    测试

--- src/main/java/com/lollipop/DemoApplication.java
+++ src/main/java/com/lollipop/DemoApplication.java
@@ -22,8 +22,10 @@ public class DemoApplication {
 			String[] beanNames = ctx.getBeanDefinitionNames();
 			Arrays.sort(beanNames);
 			for (String beanName : beanNames) {
+				System.out.println("111");
 				System.out.println(beanName);
 			}
+
 		};
 	}
 }
